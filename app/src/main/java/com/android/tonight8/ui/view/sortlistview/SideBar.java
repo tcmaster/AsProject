4ff704993d6918d1eeceb9202289488da8589240
lzclass <63993048@qq.com>
2015-07-10 12:10:42 +0800

    城市列表

--- app/src/main/java/com/android/tonight8/ui/view/sortlistview/SideBar.java
+++ app/src/main/java/com/android/tonight8/ui/view/sortlistview/SideBar.java
@@ -10,143 +10,130 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+
 import com.android.tonight8.R;
 
-/**
- * 
- * �Ҳ����ĸ����View
- * 
- * @author
- * 
- */
-
 public class SideBar extends View {
+    // 触摸事件
+    private OnTouchingLetterChangedListener onTouchingLetterChangedListener;
+    // 26个字母
+    public static String[] b = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z", "#"};
+    private int choose = -1;// 选中
+    private Paint paint = new Paint();
 
-	// �����¼�
-	private OnTouchingLetterChangedListener onTouchingLetterChangedListener;
+    private TextView mTextDialog;
 
-	// 26����ĸ
-	public static String[] b = { "A", "B", "C", "D", "E", "F", "G", "H", "I",
-			"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-			"W", "X", "Y", "Z", "#" };
-	// ѡ��
-	private int choose = -1;
+    /**
+     * 为SideBar设置显示字母的TextView
+     *
+     * @param mTextDialog
+     */
+    public void setTextView(TextView mTextDialog) {
+        this.mTextDialog = mTextDialog;
+    }
 
-	private Paint paint = new Paint();
 
-	private TextView mTextDialog;
+    public SideBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-	/**
-	 * ΪSideBar��ʾ��ĸ��TextView
-	 * 
-	 * @param mTextDialog
-	 */
-	public void setTextView(TextView mTextDialog) {
-		this.mTextDialog = mTextDialog;
-	}
+    public SideBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public SideBar(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-	}
+    public SideBar(Context context) {
+        super(context);
+    }
 
-	public SideBar(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    /**
+     * 重写这个方法
+     */
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 获取焦点改变背景颜色.
+        int height = getHeight();// 获取对应高度
+        int width = getWidth(); // 获取对应宽度
+        int singleHeight = height / b.length;// 获取每一个字母的高度
 
-	public SideBar(Context context) {
-		super(context);
-	}
+        for (int i = 0; i < b.length; i++) {
+            paint.setColor(Color.rgb(33, 65, 98));
+            // paint.setColor(Color.WHITE);
+            paint.setTypeface(Typeface.DEFAULT_BOLD);
+            paint.setAntiAlias(true);
+            paint.setTextSize(20);
+            // 选中的状态
+            if (i == choose) {
+                paint.setColor(Color.parseColor("#3399ff"));
+                paint.setFakeBoldText(true);
+            }
+            // x坐标等于中间-字符串宽度的一半.
+            float xPos = width / 2 - paint.measureText(b[i]) / 2;
+            float yPos = singleHeight * i + singleHeight;
+            canvas.drawText(b[i], xPos, yPos, paint);
+            paint.reset();// 重置画笔
+        }
 
-	/**
-	 * 
-	 * ��д��onDraw�ķ���
-	 * 
-	 */
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		int height = getHeight();// ��ȡ��Ӧ�ĸ߶�
-		int width = getWidth();// ��ȡ��Ӧ�Ŀ��
-		int singleHeight = height / b.length;// ��ȡÿһ����ĸ�ĸ߶�
-		for (int i = 0; i < b.length; i++) {
-			paint.setColor(Color.rgb(33, 65, 98));
-			paint.setTypeface(Typeface.DEFAULT_BOLD);
-			paint.setAntiAlias(true);
-			paint.setTextSize(20);
-			// ѡ�е�״̬
-			if (i == choose) {
-				paint.setColor(Color.parseColor("#3399ff"));
-				paint.setFakeBoldText(true);// �����Ƿ�Ϊ��������
-			}
-			// x������=�м�-�ַ��ȵ�һ��
-			float xPos = width / 2 - paint.measureText(b[i]) / 2;
-			float yPos = singleHeight * i + singleHeight;
-			canvas.drawText(b[i], xPos, yPos, paint);
-			paint.reset();// ���û���
-		}
-	}
+    }
 
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent event) {
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        final int action = event.getAction();
+        final float y = event.getY();// 点击y坐标
+        final int oldChoose = choose;
+        final OnTouchingLetterChangedListener listener = onTouchingLetterChangedListener;
+        final int c = (int) (y / getHeight() * b.length);// 点击y坐标所占总高度的比例*b数组的长度就等于点击b中的个数.
 
-		final int action = event.getAction();
-		final float y = event.getY();// ���y���
-		final int oldChoose = choose;
+        switch (action) {
+            case MotionEvent.ACTION_UP:
+                setBackgroundDrawable(new ColorDrawable(0x00000000));
+                choose = -1;//
+                invalidate();
+                if (mTextDialog != null) {
+                    mTextDialog.setVisibility(View.INVISIBLE);
+                }
+                break;
 
-		final OnTouchingLetterChangedListener listener = onTouchingLetterChangedListener;
+            default:
+                setBackgroundResource(R.drawable.sidebar_background);
+                if (oldChoose != c) {
+                    if (c >= 0 && c < b.length) {
+                        if (listener != null) {
+                            listener.onTouchingLetterChanged(b[c]);
+                        }
+                        if (mTextDialog != null) {
+                            mTextDialog.setText(b[c]);
+                            mTextDialog.setVisibility(View.VISIBLE);
+                        }
 
-		final int c = (int) (y / getHeight() * b.length);// ���y�����ռ�߶ȵı���*b����ĳ��Ⱦ͵��ڵ��b�еĸ���
+                        choose = c;
+                        invalidate();
+                    }
+                }
 
-		switch (action) {
-		case MotionEvent.ACTION_UP:
-			setBackgroundDrawable(new ColorDrawable(0x00000000));// ���ñ�����ɫ
-			choose = -1;
-			invalidate();
-			if (mTextDialog != null) {
-				mTextDialog.setVisibility(View.INVISIBLE);
-			}
-			break;
+                break;
+        }
+        return true;
+    }
 
-		default:
-			setBackgroundResource(R.drawable.sidebar_background);
-			if (oldChoose != c) {
-				if (c >= 0 && c < b.length) {
-					if (listener != null) {
-						listener.onTouchingLetterChanged(b[c]);
-					}
-					if (mTextDialog != null) {
-						mTextDialog.setText(b[c]);
-						mTextDialog.setVisibility(View.VISIBLE);
-					}
-					choose = c;
-					invalidate();
-				}
-			}
-			break;
-		}
+    /**
+     * 向外公开的方法
+     *
+     * @param onTouchingLetterChangedListener
+     */
+    public void setOnTouchingLetterChangedListener(
+            OnTouchingLetterChangedListener onTouchingLetterChangedListener) {
+        this.onTouchingLetterChangedListener = onTouchingLetterChangedListener;
+    }
 
-		return true;
-	}
-
-	/**
-	 * �����ɿ��ķ���
-	 * 
-	 * @param onTouchingLetterChangedListener
-	 */
-	public void setOnTouchingLetterChangedListener(
-			OnTouchingLetterChangedListener onTouchingLetterChangedListener) {
-		this.onTouchingLetterChangedListener = onTouchingLetterChangedListener;
-	}
-
-	/**
-	 * 
-	 * �ӿ�
-	 * 
-	 * @author
-	 * 
-	 */
-	public interface OnTouchingLetterChangedListener {
-		public void onTouchingLetterChanged(String s);
-	}
+    /**
+     * 接口
+     *
+     * @author coder
+     */
+    public interface OnTouchingLetterChangedListener {
+        public void onTouchingLetterChanged(String s);
+    }
 
 }
